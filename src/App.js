eca6d2f943ec3642c1c5c26cb187e507066df236
Marwan Mashaly <mmash102@uottawa.ca>
2023-07-16 21:48:21 -0400

    update app name

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -6,14 +5,13 @@ import Events from "./pages/Events";
 import Contact from "./pages/Contact";
 import Spots from "./pages/Spots";
 import PlanYourVisit from "./pages/PlanYourVisit";
-import LanguageSelector from "./components/LanguageSelector";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
           <Route path="/events" element={<Events />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/Spots" element={<Spots />} />
