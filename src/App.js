eae383db4ad1bff205ae46b50c40f9bed0023ffc
Marwan Mashaly <mmash102@uottawa.ca>
2023-07-16 22:20:01 -0400

    update routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Router } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
 import Contact from "./pages/Contact";
@@ -8,17 +8,17 @@ import PlanYourVisit from "./pages/PlanYourVisit";
 
 function App() {
   return (
-    <div className="App">
-      <HashRouter>
+    <Router>
+      <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Home />} />
           <Route path="/events" element={<Events />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/Spots" element={<Spots />} />
           <Route path="/plan-your-vist" element={<PlanYourVisit />} />
         </Routes>
-      </HashRouter>
-    </div>
+      </div>
+    </Router>
   );
 }
 
